2dc597b89f604e87da91789548e181f8c11cbb38
DavSonCZE <40635601+DavSonCZE@users.noreply.github.com>
2020-05-06 15:53:50 +0200

    Add files via upload

---
+++ preload.lua
@@ -0,0 +1,33 @@
+local isVehiclesLoaded =  false
+
+local vehicles = {
+    p1,
+    mgt,
+}
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+            if not isVehiclesLoaded  then
+                for i, name in ipairs(vehicles) do
+                    targetVehicle = name
+
+                    if not IsModelInCdimage(targetVehicle) or not IsModelAVehicle(targetVehicle) then
+                        Citizen.Trace(targetVehicle .. " could not be found as a vehicle.")
+                        return
+                    end
+
+                    RequestModel(targetVehicle)
+
+                    while not HasModeLoaded(targetVehicle) do
+                        Citizen.Trace(targetVehicle .. "[VehicleLoader] Loading vehicle " .. targetVehicle)
+                        Citizen.Wait(15000)
+                    end
+
+                    if HasModeLoaded(targetVehicle) then
+
+                    end
+                    SetModelAsNoLongerNeeded(targetVehicle)
+                end
+            end
+        end)
